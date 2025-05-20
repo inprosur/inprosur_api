44b9b925d8eecc64129928b37e555df960b19739
edd86 <edward.cristob@gmail.com>
2025-05-19 22:20:01 -0400

    feat: reorganize user routes setup in index file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes';
+
 
 dotenv.config();
 const app = express();
-
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use('/api/users', userRoutes);
 
-app.get('/', (_req, res) => {
-    res.send('Hello World! ' + new Date().toLocaleTimeString());
-});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log('API is running on port', PORT);
