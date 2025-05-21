796088553eedfa28c3258f6b293a25f31a4704d7
edd86 <edward.cristob@gmail.com>
2025-05-21 03:32:06 -0400

    fix(routes): update comments for user CRUD routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,10 +3,12 @@ import * as UserController from '../controllers/userController';
 
 const router = Router();
 
-// Define la ruta para obtener todos los usuarios
+// Define la ruta para hacer CRUD a todos los usuarios
 // GET /api/users
 router.get('/', UserController.getAllUsers);
+//GET /api/users/:id
 router.get('/:id', UserController.getUserById);
+// POST /api/users/newUser
 router.post('/newUser', UserController.createUser);
 
 export default router;
