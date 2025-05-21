00f26eab7ad6a8a44421300dd02342d17d780f37
edd86 <edward.cristob@gmail.com>
2025-05-21 03:31:25 -0400

    fix(routes): add permission routes to the API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes';
+import permissionRoutes from './routes/permissionRoutes';;
 
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/permissions', permissionRoutes);
 
 const PORT = process.env.PORT || 3000;
 
