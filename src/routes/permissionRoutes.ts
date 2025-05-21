6a03a61a23b39305869e33a180daf7438f734a21
edd86 <edward.cristob@gmail.com>
2025-05-21 03:31:55 -0400

    feat(permission): add permission routes for CRUD operations

---
+++ src/routes/permissionRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as PermissionController from '../controllers/permissionController';
+
+const router = Router();
+
+// Define las rutas para hacer CRUD a todos los permisos
+// GET /api/permissions
+router.get('/', PermissionController.getAllPermissions);
+// GET /api/permissions/:id
+router.get('/:id', PermissionController.getPermissionById);
+// POST /api/permissions/newPermission
+router.post('/newPermission', PermissionController.createPermission);
+
+export default router;
