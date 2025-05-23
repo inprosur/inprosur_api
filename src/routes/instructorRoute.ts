924f9c53f723b1a3011076c981b5105028dd4131
edd86 <edward.cristob@gmail.com>
2025-05-23 17:02:21 -0400

    feat: add instructor route with CRUD operations

---
+++ src/routes/instructorRoute.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import * as InstructorController from "../controllers/instructorController";
+
+const router = Router();
+
+router.post("/newInstructor", InstructorController.createInstructor);
+router.get("/", InstructorController.getAllInstructors);
+router.get("/:id", InstructorController.getInstructorById);
+
+export default router;
