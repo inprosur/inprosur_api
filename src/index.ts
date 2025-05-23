f8b3b2ea2f0a121e12e5f5c69928dc7cccfcb61f
edd86 <edward.cristob@gmail.com>
2025-05-22 21:46:53 -0400

    fix(index): standardize import formatting and add roleRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes';
-import permissionRoutes from './routes/permissionRoutes';;
-
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import permissionRoutes from "./routes/permissionRoutes";
+import roleRoutes from "./routes/roleRoutes";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/permissions', permissionRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/permissions", permissionRoutes);
+app.use("/api/roles", roleRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('API is running on port', PORT);
-    console.log('TURSO_URL:', process.env.TURSO_URL);
-})
+  console.log("API is running on port", PORT);
+  console.log("TURSO_URL:", process.env.TURSO_URL);
+});
