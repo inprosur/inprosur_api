1af82ebac44da28e3c9ddad46609eeb4fa425701
edd86 <edward.cristob@gmail.com>
2025-05-23 17:02:41 -0400

    feat: implement instructor service with CRUD operations

---
+++ src/services/instructorService.ts
@@ -0,0 +1,35 @@
+import db from "../config/db";
+import { Instructor } from "../models/Instructor";
+
+export const createInstructor = async (
+  instructor: Instructor
+): Promise<Instructor> => {
+  const result = await db.execute(
+    "INSERT INTO Instructors (name, biography, phone, userId) VALUES",
+    [instructor.name, instructor.biography, instructor.phone, instructor.userId]
+  );
+  const id = result.lastInsertRowid;
+  const row = {
+    ...instructor,
+    id: id !== undefined ? Number.parseInt(id.toString()) : undefined,
+  };
+  return row as Instructor;
+};
+
+export const getAllInstructors = async (): Promise<Instructor[]> => {
+  const result = await db.execute("SELECT * FROM Instructors");
+  const rows = Array.isArray(result) ? result[0] : result.rows;
+  return rows as Instructor[];
+};
+
+export const getInstructorById = async (
+  id: number
+): Promise<Instructor | null> => {
+  const result = await db.execute("SELECT * FROM Instructors WHERE id=?", [id]);
+  const rows = Array.isArray(result) ? result[0] : result.rows;
+  if (rows.length === 1) {
+    return rows[0] as Instructor;
+  } else {
+    return null;
+  }
+};
