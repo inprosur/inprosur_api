9520d7ba7ee7a6320f7fd0576ec492c68c95bd8a
edd86 <edward.cristob@gmail.com>
2025-05-23 02:29:56 -0400

    refactor(roleController): enhance error handling and response messages in createRole and getAllRoles functions

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -5,15 +5,17 @@ import * as RolePermissionService from "../services/rolPermissionService";
 // Función para agregar un nuevo rol usando el servicio de rol. Se espera que el cuerpo de la solicitud contenga las IDs de Rol y Permiso. Se maneja un error 400 en caso de que falten los IDs.
 export const createRole = async (req: Request, res: Response) => {
   try {
+    // Verifica si el cuerpo de la solicitud está vacío y devuelve un error 400 si es así
     if (!req.body) {
       res.status(400).json({
         error: "Request body is missing",
+        message: "Body is missing. Make sure to use express.json().",
       });
       return;
     }
 
+    // Verifica si el cuerpo de la solicitud contiene los campos requeridos: name, descirption y permissionId. Si falta alguno de ellos, devuelve un error 400
     const { name, description, permissionId } = req.body;
-
     if (!name || !description || !permissionId) {
       res.status(400).json({
         error: "Missing required fields.",
@@ -22,33 +24,17 @@ export const createRole = async (req: Request, res: Response) => {
       return;
     }
 
-    await RoleService.createRole({ name, description });
-    const newRole = await RoleService.getRoleByName(name);
-    if (!newRole) {
-      res.status(400).json({
-        error: "Failed to create role",
-        message: "Role was not created or not found",
-      });
-      return;
-    }
-
-    await RolePermissionService.createRolePermission({
-      roleId: newRole!.id!,
-      permissionId: permissionId,
+    const newRole = await RoleService.createRole({
+      name,
+      description,
     });
-    const newRolePermission =
-      await RolePermissionService.getRollPermissionByIds({
-        roleId: newRole!.id!,
-        permissionId: permissionId,
-      });
-    if (!newRolePermission) {
-      res.status(400).json({
-        error: "Failed to create role-permisssion relationship",
-        message: "Role-permission relationship was not created or not found",
-      });
-      return;
-    }
 
+    const newRolePermission = await RolePermissionService.createRolePermission({
+      roleId: newRole!.id!,
+      permissionId,
+    });
+
+    // Si todo sale bien, devuelve un mensaje de éxito y el nuevo rol y la relación de rol-permission
     res.status(201).json({
       successs: true,
       data: {
@@ -58,10 +44,36 @@ export const createRole = async (req: Request, res: Response) => {
       message: "Role and role-permission relationship created succsessfully",
     });
   } catch (error) {
+    // Maneja cualquier error que ocurra durante el proceso de creación del rol y la relación de rol-permission. Devuelve un error 500 si ocurre un error interno del servidor
     console.error("Error creating role:", error);
     res.status(500).json({
       error: "Internal server error",
       message: "Failed to create role and role-permission relationship",
+    });
+  }
+};
+
+// Función para obtener todos los roles usando el servicio del rolService. Se maneja un error 404 si no se encuentran roles en la base de datos.
+export const getAllRoles = async (_req: Request, res: Response) => {
+  try {
+    const roles = await RoleService.getAllRoles();
+    if (!roles || roles.length === 0) {
+      res.status(404).json({
+        error: "No roles found",
+        message: "No roles found in the database.",
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      data: roles,
+      message: "Roles retrieved successfully.",
+    });
+  } catch (error) {
+    console.error("Error retrieving roles:", error);
+    res.status(500).json({
+      error: "Internal server error",
+      message: "Failed to retrieve roles.",
     });
   }
 };
