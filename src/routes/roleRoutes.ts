9bae645a9b5ef6b84aaedf0c47576b514712ebd8
edd86 <edward.cristob@gmail.com>
2025-05-23 17:02:31 -0400

    feat: add routes for retrieving all roles and role by ID

--- src/routes/roleRoutes.ts
+++ src/routes/roleRoutes.ts
@@ -6,5 +6,7 @@ const router = Router();
 // Define la ruta para hacer CRUD a la tabla roles
 // Post /api/roles/newRole
 router.post("/newRole", RoleController.createRole);
+router.get("/", RoleController.getAllRoles);
+router.get("/:id", RoleController.getRoleById);
 
 export default router;
