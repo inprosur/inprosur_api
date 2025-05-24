cb94a05cf3fab2401c03bc4b6c0c9a3ff0210865
edd86 <edward.cristob@gmail.com>
2025-05-24 04:01:52 -0400

    feat: implement course controller with create, get all, and get by ID functions

---
+++ src/controllers/courseController.ts
@@ -0,0 +1,120 @@
+import { Request, Response } from "express";
+import * as CourseService from "../services/courseService";
+
+export const createCourse = async (req: Request, res: Response) => {
+  try {
+    if (!req.body) {
+      res.status(400).json({
+        error: "Body is missing",
+        message:
+          "Request body is missing. Make sure to use express.json() middlewre.",
+      });
+      return;
+    }
+
+    const {
+      title,
+      description,
+      instructorId,
+      categoryId,
+      subcategoryId,
+      price,
+      isPublished,
+      duration,
+      thumbnailUrl,
+    } = req.body;
+
+    if (!title || !description || !instructorId || !price || !isPublished) {
+      res.status(400).json({
+        error: "Missing required fields",
+        message:
+          "title, description, instructorId, price, isPublished are required",
+      });
+      return;
+    }
+
+    const newCourse = await CourseService.createCourse({
+      title,
+      description,
+      instructorId,
+      categoryId: categoryId || null,
+      subcategoryId: subcategoryId || null,
+      price,
+      isPublished,
+      duration: duration || null,
+      thumbnailUrl: thumbnailUrl || null,
+      creationDate: new Date(),
+    });
+
+    res.status(201).json({
+      success: true,
+      data: newCourse,
+      message: "Course created successfully.",
+    });
+  } catch (error) {
+    console.log("Error creating user: ", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+      message: "Failed to create the Course.",
+    });
+  }
+};
+
+export const getAllCourses = async (_req: Request, res: Response) => {
+  try {
+    const courses = await CourseService.getAllCourses();
+    if (!courses || courses.length === 0) {
+      res.status(404).json({
+        error: "No courses found",
+        message: "No courses found in the database.",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data: courses,
+      message: "Courses retrieved successfully.",
+    });
+  } catch (error) {
+    console.error("Error fetching courses: ", error);
+    res.status(500).json({
+      error: "Internal server error",
+      message: "Failed to fetch courses.",
+    });
+  }
+};
+
+export const getCourseById = async (req: Request, res: Response) => {
+  try {
+    const courseId = parseInt(req.params.id);
+    if (!courseId) {
+      res.status(400).json({
+        error: "Bad request",
+        message: "Course ID is required.",
+      });
+      return;
+    }
+
+    const course = await CourseService.getCourseById(courseId);
+    if (!course) {
+      res.status(404).json({
+        error: "Not found",
+        message: "Course not found.",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data: course,
+      message: "Course retrieved successfully.",
+    });
+  } catch (error) {
+    console.log("Error fetching course: ", error);
+    res.status(500).json({
+      error: "Internal server error",
+      message: "Failed to fetch course.",
+    });
+  }
+};
