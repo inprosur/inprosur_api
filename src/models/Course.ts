6c397ad079359e0c2f6d8389da3a27c810524c4c
edd86 <edward.cristob@gmail.com>
2025-05-24 04:01:25 -0400

    fix: correct property name and type for Course model

--- src/models/Course.ts
+++ src/models/Course.ts
@@ -1,13 +1,13 @@
 export interface Course {
-    id?: number;
-    title: string;
-    description: string;
-    instructorId: number;
-    categoryId?: number;
-    subcategoryId?: number;
-    price: number;
-    isPublished: boolean;
-    duration: number;
-    thumbnailUrl: string;
-    creation_date?: Date;
+  id?: number;
+  title: string;
+  description: string;
+  instructorId: number;
+  categoryId?: number;
+  subcategoryId?: number;
+  price: number;
+  isPublished: boolean;
+  duration?: string;
+  thumbnailUrl?: string;
+  creationDate: Date;
 }
