66917148ed0033a73d512d4b2884d01715c847c7
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2025-06-04 17:35:43 -0400

    feat: implement advertising service with functions to get and create advertisings

---
+++ src/services/advertisingService.ts
@@ -0,0 +1,16 @@
+import db from '../config/db';
+import { Advertising } from '../models/Advertising';
+
+export const getAllAdvertisings = async () => {
+    const result = await db.execute('SELECT * FROM Advertisings');
+    const rows = Array.isArray(result) ? result[0] : result.rows;
+    return rows as Advertising[];
+}
+
+export const createAdvertising = async (advertising: Advertising): Promise<Advertising> => {
+    const result = await db.execute('INSERT INTO Advertisings (imgUrl, externalUrl, status, courseId, createdAt) VALUES (?,?,?,?,?)',
+        [advertising.imgUrl, advertising.externalUrl!, advertising.status, advertising.courseId, advertising.createdAt.toISOString()]);
+    const id = result.lastInsertRowid;
+    const row = { ...advertising, id: id !== undefined ? Number.parseInt(id.toString()) : undefined };
+    return row as Advertising;
+}
