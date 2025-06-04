9e5eba06023618c92dc88ba75aa1a64780f9c535
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2025-06-04 17:58:59 -0400

    feat: add advertising routes for retrieving and creating advertisings

---
+++ src/routes/advertisingRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as AdvertisingController from '../controllers/advertisingController';
+
+const router = Router();
+
+router.get("/", AdvertisingController.getAllAdvertisings);
+router.post("/", AdvertisingController.createAdvertising);
+
+export default router;
