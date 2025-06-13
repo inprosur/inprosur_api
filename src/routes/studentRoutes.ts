7d1bf5e405bd7e477122c83a2cd482d22803f67b
edd86 <edward.cristob@gmail.com>
2025-06-13 10:15:48 -0400

    feat: add route to retrieve student by user ID in student management

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post("/newStudent", StudentController.createStudent);
 router.get("/", StudentController.getAllStudents);
 router.get("/:id", StudentController.getStudentById);
+router.get("/", StudentController.getStudentByUserId);
 
 export default router;
