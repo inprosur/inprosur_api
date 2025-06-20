41cde4f57c4e774e0804a6ae4cb0c805afaa4f48
edd86 <edward.cristob@gmail.com>
2025-06-20 18:20:19 -0400

    feat: add comparePassword function for password verification

--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -6,3 +6,10 @@ dotenv.config();
 export const hashedPassword = async (password: string): Promise<string> => {
   return await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS!));
 };
+
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hashedPassword);
+};
