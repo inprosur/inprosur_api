bb87e9f2c0abca8c7ad6fdc71352ca7ab736625b
edd86 <edward.cristob@gmail.com>
2025-07-19 15:11:24 -0400

    refactor: remove user-related controller, service, model, and route files

--- src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as UserController from "../controllers/userController";
-
-const router = Router();
-
-// Solo la ruta de creación de usuarios
-router.post("/newUser", UserController.createUser);
-router.get("/", UserController.getAllUsers);
-export default router;
