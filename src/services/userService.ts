f32fb3e3a5d4e1018e10d4c8898cd64bbb81afbb
edd86 <edward.cristob@gmail.com>
2025-07-19 16:46:15 -0400

    refactor(userService): remove password hashing from createUser
    The password hashing is now handled at a different layer, so we can simplify the user creation service by removing this responsibility.

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,16 +1,16 @@
 import { getTursoClient } from "../config/db";
 import { User } from "../models/User";
-import { hashedPassword } from "../utils/hashPassword";
+//import { hashedPassword } from "../utils/hashPassword";
 
 export const createUser = async (user: User): Promise<User> => {
   const turso = getTursoClient();
-  const newPassword = await hashedPassword(user.password);
+
   const result = await turso.execute({
     sql: "INSERT INTO Users (username, email, password, createdAt, uId, photo, status) VALUES (?, ?, ?, ?, ?, ?, ?)",
     args: [
       user.username,
       user.email,
-      newPassword,
+      user.password,
       user.createdAt.toISOString(),
       user.uId,
       user.photo,
