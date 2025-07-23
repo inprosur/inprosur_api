090affa91f6d9cd31fb4627b1dd5305186492a66
rodolfo <tinchoevil@gmail.com>
2025-07-23 19:03:41 -0400

    feat: refresh newDegree

--- src/routes/degreeRoutes.ts
+++ src/routes/degreeRoutes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get("/", DegressController.getAllDegrees);
 router.get("/:id", DegressController.getDegreesById);
+//nuevo degree
 router.post("/newDegree", DegressController.createDegrees);
 
 export default router;
