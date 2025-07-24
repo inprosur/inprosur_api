14215dd0264f8ea27adc2919731f0e24f69df4ad
tincho <tinchoevil@gmail.com>
2025-07-23 20:22:14 -0400

    feat: actualizacion newRole and newDegree in their routes

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -6,6 +6,6 @@ const router = Router();
 router.get("/", CategoryController.getAllCategories);
 router.get("/degree", CategoryController.getCategoryByDegreeId);
 router.get("/:id", CategoryController.getCategoryById);
-router.post("/", CategoryController.createCategory);
+router.post("/newCategory", CategoryController.createCategory);
 
 export default router;
