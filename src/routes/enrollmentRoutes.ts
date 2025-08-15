52619b9028e31a009b643f476a0e1b5583519066
edd86 <edward.cristob@gmail.com>
2025-08-15 19:19:55 -0400

    fix: reorder routes in enrollmentRoutes for better organization

--- src/routes/enrollmentRoutes.ts
+++ src/routes/enrollmentRoutes.ts
@@ -4,11 +4,11 @@ import * as EnrollmentController from "../controllers/enrollmentController";
 const router = Router();
 
 router.get("/", EnrollmentController.getAllEnrollments);
-router.get("/:id", EnrollmentController.getEnrollmentById);
 router.get(
   "/studentEnrolledInCourse",
   EnrollmentController.studentEnrolledInCourse
 );
+router.get("/:id", EnrollmentController.getEnrollmentById);
 router.post("/", EnrollmentController.createEnrollment);
 router.get("/studentCourses", EnrollmentController.getStudentCourses);
 
